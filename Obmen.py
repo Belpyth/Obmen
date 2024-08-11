3ef5cf5cbb115914cdfcc17291561d22751ba537
Belpyth <bel.pyth@gmail.com>
2024-08-11 16:00:02 +0300

    Создано функция для получения из интенрета курса валют

--- Obmen.py
+++ Obmen.py
@@ -7,6 +7,20 @@ from tkinter import messagebox as mb
 
 def exchange():
     code = entry.get().upper()
+    if code:
+        try:
+            resource = requests.get('https://open.er-api.com/v6/latest/USD')
+            resource.raise_for_status()
+            data = resource.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo('Курс обмена', f"Курс {exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта с кодом {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка {e}")
+    else:
+        mb.showwarning("Внимание", "Введите код валюты")
 
 
 window = Tk()
